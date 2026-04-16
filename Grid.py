09ad299676c1fe21d07f09a54fa69c076100cf79
anoukavontuur <anouk.avontuur@hotmail.com>
2026-04-16 15:34:08 +0200

    Kleurtjes

--- Grid.py
+++ Grid.py
@@ -5,7 +5,7 @@ class GridGraph:
         self.all_nodes = [(x, y) for x in range(width) for y in range(height)]
 
     def neighbors(self, node, current_time, goal_xy):
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)] 
+        directions = [(0, 0),(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)] 
         result = []
         
         for dx, dy in directions:
